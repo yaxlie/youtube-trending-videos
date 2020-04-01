965ae07b7f621250813ce97b181e2cabfecdfb60
yaxlie <m.lesnierowski@gmail.com>
2020-04-01 02:04:45 +0200

    Add simple finders

--- src/find.py
+++ src/find.py
@@ -1,6 +1,24 @@
 from finders import *
 
 if __name__ == "__main__":
-    reader = None
-    with UpperCaseFinder('youtube_data/GB_videos_5p.csv', 'title', reader) as finder:
-        print(f'"{finder.name}"";"{finder.column_name}"";{finder.found};{finder.total}')
+    data = None
+    txt_columns=['title', 'channel_title', 'tags', 'description']
+
+
+    # TODO: Reflection in python? Instead of multiple loops make list of classes and use it in one loop
+    for column in txt_columns:
+        with UpperCaseFinder('youtube_data/GB_videos_5p.csv', column, data) as finder:
+            print(f'"{finder.name}"";"{finder.column_name}"";{finder.found};{finder.total};{finder.missing}')
+            reader = finder.data
+
+    for column in txt_columns:
+        with LowerCaseFinder('youtube_data/GB_videos_5p.csv', column, data) as finder:
+            print(f'"{finder.name}"";"{finder.column_name}"";{finder.found};{finder.total};{finder.missing}')
+    
+    for column in txt_columns:
+        with DigitsFinder('youtube_data/GB_videos_5p.csv', column, data) as finder:
+            print(f'"{finder.name}"";"{finder.column_name}"";{finder.found};{finder.total};{finder.missing}')
+    
+    for column in txt_columns:
+        with ContainSpecialCharsFinder('youtube_data/GB_videos_5p.csv', column, data) as finder:
+            print(f'"{finder.name}"";"{finder.column_name}"";{finder.found};{finder.total};{finder.missing}')
--- src/finders.py
+++ src/finders.py
@@ -1,19 +1,21 @@
 from abc import ABC, abstractmethod
 import csv
+import re
 
 class AttributeFinder(ABC):
-    def __init__(self, file_name: str, column_name: str, reader,  name: str = __name__):
+    def __init__(self, file_name: str, column_name: str, data,  name: str = __name__):
         self.name = name
         self.file_name = file_name
-        self.column_name = column_name
+        self.column_name = column_name.strip()
         self.total = 0
         self.found = 0
-        self.data = None
+        self.missing = 0
+        self.data = data
     
     def __enter__(self):
         if not self.data:
-            with open(self.file_name, newline='', encoding='utf-8', errors='ignore') as f:
-                self.data = [{key: value for key, value in row.items()} for row in csv.DictReader(f, skipinitialspace=True, delimiter=';')]
+            with open(self.file_name, newline='', encoding='utf-8', errors='replace') as f:
+                self.data = [{key.strip(): value for key, value in row.items()} for row in csv.DictReader(f, skipinitialspace=True, delimiter=';')]
         self.total = len(self.data)
         self.count()
 
@@ -28,6 +30,10 @@ class AttributeFinder(ABC):
 
     def count(self):
         for row in self.data:
+            if not self.column_name in row or any(row[self.column_name] == x for x in [None, '']):
+                self.missing += 1
+                continue
+
             if self.is_condition_met(row[self.column_name]):
                 self.found += 1
 
@@ -35,8 +41,29 @@ class AttributeFinder(ABC):
         return self.name
 
 class UpperCaseFinder(AttributeFinder):
-    def __init__(self, file_name: str, column_name: str, reader):
-        super().__init__(file_name, column_name, reader, 'Upper Case')
+    def __init__(self, file_name: str, column_name: str, data):
+        super().__init__(file_name, column_name, data, 'Upper case')
 
     def is_condition_met(self, data: str) -> bool:
         return data.isupper()
+
+class LowerCaseFinder(AttributeFinder):
+    def __init__(self, file_name: str, column_name: str, data):
+        super().__init__(file_name, column_name, data, 'Lower case')
+
+    def is_condition_met(self, data: str) -> bool:
+        return data.islower()
+
+class DigitsFinder(AttributeFinder):
+    def __init__(self, file_name: str, column_name: str, data):
+        super().__init__(file_name, column_name, data, 'With digits')
+
+    def is_condition_met(self, data: str) -> bool:
+        return re.match(r'\d', data)
+
+class ContainSpecialCharsFinder(AttributeFinder):
+    def __init__(self, file_name: str, column_name: str, data):
+        super().__init__(file_name, column_name, data, 'With special characters')
+
+    def is_condition_met(self, data: str) -> bool:
+        return re.match(r'[^a-zA-Z.? !0-9]', data)
